e293a8a282ac09b2d2f10a63ec55a2e889ab902c
hangim <hangim@users.noreply.github.com>
2020-06-26 16:48:10 +0800

    Add encoding declaration and remove .dict.yaml suffix.
    Remove unnecessary .dict.yaml suffix because rime will autonomously add it.
    Add single character table(单字码表).

--- convert.py
+++ convert.py
@@ -9,7 +9,7 @@ converter = opencc.OpenCC('s2t.json')
 FILE = sys.argv[1]
 
 HANZI_RE = re.compile('^[\u4e00-\u9fa5]+$')
-YAML_HEADER = "---\nname: luna_pinyin.zhwiki.dict.yaml\nversion: \"2020.05.23\"\nsort: by_weight\nuse_preset_vocabulary: true\n...\n"
+YAML_HEADER = "# encoding: utf-8\n\n---\nname: luna_pinyin.zhwiki\nversion: \"2020.05.29\"\nsort: by_weight\nuse_preset_vocabulary: true\nimport_tables:\n  - luna_pinyin\n...\n"
 print(YAML_HEADER)
 count = 0
 with open(FILE) as f:
